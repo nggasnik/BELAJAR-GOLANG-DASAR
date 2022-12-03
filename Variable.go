9920d126583cacd6a6718eed8171bedfdec907a8
Erlangga <110957992+nggasnik@users.noreply.github.com>
2022-12-03 21:54:42 +0700

    Create Variable.go

---
+++ Variable.go
@@ -0,0 +1,17 @@
+package main 
+
+import "fmt"
+
+func main() {
+  var name string
+  
+  name = "Erlangga"
+  fmt.Println(name)
+  
+  var (
+        firstName = "Erlangga"
+        lastName = "Doang"
+  )
+    fmt.Println(firstName)
+    fmt.Println(lastName)
+}
