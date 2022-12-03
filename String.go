8db6580b779b8bf33e53bf45fa65ea21252c3963
Erlangga <110957992+nggasnik@users.noreply.github.com>
2022-12-03 21:45:47 +0700

    Create String.go

---
+++ String.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Erlangga")
+  fmt.Println(len("Erlangga"))
+  fmt.Println("Erlangga"[0]) //FUNGSI [0] <= Untuk mengambil karakter , karakter pertama dimulai dari 0 dst
+}
