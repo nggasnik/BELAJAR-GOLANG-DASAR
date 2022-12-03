4bb37b9a8ee83e081eaab2fe8b1642525e7c9f69
Erlangga <110957992+nggasnik@users.noreply.github.com>
2022-12-03 21:31:14 +0700

    Update Number.go

--- Number.go
+++ Number.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Satu = " 1)
-  fmt.Println("Dua = " 2)
-  fmt.Println("DUA KOMA LIMA = " 2.5)
+  fmt.Println("Satu = ", 1)
+  fmt.Println("Dua = ", 2)
+  fmt.Println("DUA KOMA LIMA = ", 2.5)
 }
